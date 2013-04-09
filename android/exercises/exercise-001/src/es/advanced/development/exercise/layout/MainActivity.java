0ee2a40802c1c736ba6ea9bf0f1b99678b80926b
Ricardo García Fernández <ricardogarfe@gmail.com>
2013-04-09 23:44:38 +0200

    Scroll layout solution to exercise 001.

--- android/exercises/exercise-001/src/es/advanced/development/exercise/layout/MainActivity.java
+++ android/exercises/exercise-001/src/es/advanced/development/exercise/layout/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.relative_layout);
+        setContentView(R.layout.scroll_layout);
     }
 
     @Override
