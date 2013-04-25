25a69e75c55ccbdb4c58290610d3eab4721b9fa2
Ricardo García Fernández <ricardogarfe@gmail.com>
2013-04-25 18:18:19 +0200

    Extender la actividad MapActivity en MapsActivity e implementar el método 'isRouteDisplayed'. Activar Zoom y clickable para poder movernos y hacer zoom en el mapa.

--- android/exercises/GoogleMapsExercise/src/es/advantage/development/maps/MapsActivity.java
+++ android/exercises/GoogleMapsExercise/src/es/advantage/development/maps/MapsActivity.java
@@ -1,15 +1,27 @@
 package es.advantage.development.maps;
 
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
 
-public class MapsActivity extends Activity {
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
+public class MapsActivity extends MapActivity {
+
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        // Obtener mapView para configurar sus valores.
+        mapView = (MapView) findViewById(R.id.mapView);
+
+        // Aplicar Zoom y clickable
+        mapView.setBuiltInZoomControls(true);
+        mapView.setClickable(true);
+
     }
 
     @Override
@@ -17,6 +29,12 @@ public class MapsActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_maps, menu);
         return true;
+    }
+
+    @Override
+    protected boolean isRouteDisplayed() {
+        // TODO Auto-generated method stub
+        return false;
     }
 
 }
