cde2e91122eba018aa8abe31c8383cedecd43b0f
Ricardo García Fernández <ricardogarfe@gmail.com>
2013-05-09 18:00:11 +0200

    Asociar el comportamiento de los botones para inicar y parar el servicio.

--- android/exercises/AndoriodServices/src/es/advantage/development/androidservices/MainActivity.java
+++ android/exercises/AndoriodServices/src/es/advantage/development/androidservices/MainActivity.java
@@ -1,15 +1,47 @@
 package es.advantage.development.androidservices;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    private Button buttonStartService;
+    private Button buttonStopService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonStartService = (Button) this
+                .findViewById(R.id.buttonStartService);
+
+        buttonStartService.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                startService(new Intent(getApplicationContext(),
+                        AndroidService.class));
+            }
+        });
+
+        buttonStopService = (Button) this.findViewById(R.id.buttonStopService);
+
+        buttonStopService.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                stopService(new Intent(getApplicationContext(),
+                        AndroidService.class));
+            }
+        });
+
     }
 
     @Override
